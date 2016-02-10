bf0929796f998b626d9764bfc1e1fbeac5e3aebc
Jamal Moir <jamal.moir@gmail.com>
2016-02-10 12:37:30 +0900

    Add nonincreasing insertion sort

--- sorting.py
+++ sorting.py
@@ -1,11 +1,20 @@
 class Sort:
 
-    def insertion_sort(self, array):
-        for j in range(1, len(array)):
-            key = array[j]
-            i = j - 1
-            while i >= 0 and array[i] > key:
-                array[i + 1] = array[i]
-                i = i - 1
-            array[i+1] = key
+    def insertion_sort(self, array, order):
+        if order > 0:
+            for j in range(1, len(array)):
+                key = array[j]
+                i = j - 1
+                while i >= 0 and array[i] > key:
+                    array[i + 1] = array[i]
+                    i = i - 1
+                array[i + 1] = key
+        elif order < 0:
+            for j in range(len(array) - 1, -1, -1):
+                key = array[j]
+                i = j + 1
+                while i < len(array) and array[i] > key:
+                    array[i - 1] = array[i]
+                    i = i + 1
+                array[i - 1] = key
         return array
